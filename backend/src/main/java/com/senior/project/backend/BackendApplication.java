a6f37a9fc2c2d1e06c7e4f31850ed56dd6408d57
James Logan <james@dbej.net>
2023-10-01 13:37:39 -0400

    added frontend with SPA and REST API functionality

--- backend/src/main/java/com/senior/project/backend/BackendApplication.java
+++ backend/src/main/java/com/senior/project/backend/BackendApplication.java
@@ -3,12 +3,17 @@ package com.senior.project.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.WebFilter;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -19,9 +24,9 @@ public class BackendApplication {
 
     @Bean
     public RouterFunction<ServerResponse> routes(PostHandler postHandler) {
-        return route(GET("/posts"), postHandler::all)
-                .andRoute(POST("/posts"), postHandler::create)
-                .andRoute(GET("/posts/{id}"), postHandler::get);
+        return route(GET("/api/posts"), postHandler::all)
+                .andRoute(POST("/api/posts"), postHandler::create)
+                .andRoute(GET("/api/posts/{id}"), postHandler::get);
 //                .andRoute(PUT("/posts/{id}"), postHandler::update)
 //                .andRoute(DELETE("/posts/{id}"), postHandler::delete);
     }
---
+++ frontend/crd/src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    serverPort: 8080
+};
