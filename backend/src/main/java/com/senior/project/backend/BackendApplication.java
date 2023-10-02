0d3113460e7807c6f074d195e42fb3e65a8d0d79
James Logan <james@dbej.net>
2023-10-02 11:53:25 -0400

    cleanup

--- backend/src/main/java/com/senior/project/backend/BackendApplication.java
+++ backend/src/main/java/com/senior/project/backend/BackendApplication.java
@@ -3,17 +3,12 @@ package com.senior.project.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
-import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import org.springframework.web.server.WebFilter;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-
-import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class BackendApplication {
