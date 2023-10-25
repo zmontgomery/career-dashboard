af35a6c2d640e1fe0957d5cdae2de0ce46203350
Justin Swistak <jts7382@rit.edu>
2023-10-25 17:27:33 -0400

    forgot to include the new files

---
+++ frontend/crd/src/app/milestones-page/milestones-page.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MilestonesPageComponent } from './milestones-page.component';
+
+@NgModule({
+  declarations: [
+    MilestonesPageComponent,
+  ],
+  imports: [
+    CommonModule,
+  ],
+})
+export class MyModule { }
