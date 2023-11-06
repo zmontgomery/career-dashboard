b1d368764ec9a1c1e1ff3296b785c4eaabe28421
Justin Swistak <jts7382@rit.edu>
2023-11-06 10:27:06 -0500

    More control to the logo

--- frontend/crd/src/app/oswego-logo/oswego-logo.component.ts
+++ frontend/crd/src/app/oswego-logo/oswego-logo.component.ts
@@ -10,6 +10,18 @@ export class OswegoLogoComponent {
   @Input()
   sizeFactor = 1;
 
+  @Input()
+  iconPosition: 'vertical' | 'horizontal' = 'horizontal';
+
+  @Input()
+  color: 'green' | 'black' | 'white' = 'green';
+
+  get imageStr() {
+    return `assets/images/Oswego_logo_${this.iconPosition}_${this.color}.png`;
+  }
+
+
   get width() { return 600 * this.sizeFactor }
   get height() { return 210 * this.sizeFactor }
+
 }
