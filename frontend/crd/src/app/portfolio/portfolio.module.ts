0dc2fa045b8f693ed5e76ceb0ad0dbd04016ce31
Justin Swistak <jts7382@rit.edu>
2023-11-06 15:56:47 -0500

    added button for when there is no resume to display

--- frontend/crd/src/app/portfolio/portfolio.module.ts
+++ frontend/crd/src/app/portfolio/portfolio.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from "@angular/material/card";
 import {PortfolioComponent} from "./portfolio.component";
 import {MilestonesModule} from "../milestones-page/milestones/milestones.module";
-
-
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -12,10 +12,12 @@ import {MilestonesModule} from "../milestones-page/milestones/milestones.module"
   ],
   exports: [
   ],
-  imports: [
-    CommonModule,
-    MatCardModule,
-    MilestonesModule
-  ]
+    imports: [
+      CommonModule,
+      MatCardModule,
+      MilestonesModule,
+      MatButtonModule,
+      MatIconModule
+    ]
 })
 export class PortfolioModule { }
