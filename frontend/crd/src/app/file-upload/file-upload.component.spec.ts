8254dfab6aa96552ee6a8301877f6c99ec43e662
Justin Swistak <jts7382@rit.edu>
2023-11-08 20:22:25 -0500

    fixed FE tests

--- frontend/crd/src/app/file-upload/file-upload.component.spec.ts
+++ frontend/crd/src/app/file-upload/file-upload.component.spec.ts
@@ -1,15 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FileUploadComponent } from './file-upload.component';
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from "@angular/material/dialog";
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {MatIconModule} from "@angular/material/icon";
 
 describe('FileUploadComponent', () => {
   let component: FileUploadComponent;
   let fixture: ComponentFixture<FileUploadComponent>;
+  let httpMock: HttpTestingController;
+  let matDialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FileUploadComponent]
+      imports: [
+        NoopAnimationsModule,
+        HttpClientTestingModule,
+        MatDialogModule,
+        MatIconModule,
+      ],
+      declarations: [FileUploadComponent],
+      providers: [
+        {provide: MAT_DIALOG_DATA, useValue: { url: "test-url"} },
+        { provide: MatDialogRef, useValue: matDialogRef },
+      ]
     });
+    httpMock = TestBed.inject(HttpTestingController);
     fixture = TestBed.createComponent(FileUploadComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- frontend/crd/src/app/portfolio/portfolio.component.spec.ts
+++ frontend/crd/src/app/portfolio/portfolio.component.spec.ts
@@ -3,6 +3,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PortfolioComponent } from './portfolio.component';
 import {MockComponent} from "ng-mocks";
 import {MilestonesComponent} from "../milestones-page/milestones/milestones.component";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
 
 describe('PortfolioComponent', () => {
   let component: PortfolioComponent;
@@ -10,7 +13,11 @@ describe('PortfolioComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PortfolioComponent, MockComponent(MilestonesComponent)]
+      declarations: [
+        PortfolioComponent,
+        MockComponent(MilestonesComponent),
+      ],
+      imports: [MatDialogModule, MatCardModule, MatIconModule]
     });
     fixture = TestBed.createComponent(PortfolioComponent);
     component = fixture.componentInstance;
