175de69be34d7908359b80606dbe1912539640ff
bml2238 <bml2238@rit.edu>
2023-11-09 19:06:44 -0500

    event carousel using events from our API

--- frontend/crd/src/app/dashboard/events/events.component.ts
+++ frontend/crd/src/app/dashboard/events/events.component.ts
@@ -9,14 +9,33 @@ import {Event} from "../../../domain/Event";
 })
 export class EventsComponent implements OnInit{
 
+  slides: any[] = [];
+  itemsPerSlide = 2; //most likely this would be passed in the API call
+  singleSlideOffset = false;
+  noWrap = false;
+  eventPage: number = 0;
+
   constructor(
     private eventService: EventService,
   ) {
   }
 
   ngOnInit() {
+    //const isMobile = navigator.userAgent; //only display one event per page on mobile
+
     this.eventService.getEvents().subscribe((events: Event[]) => {
       this.events = events;
+
+      this.slides = new Array(events.length).fill({id: -1, name: '', date: '', img: ''});
+      this.slides.forEach((slide, index) => {
+        this.slides[index] = {
+          id: events[index].eventID,
+          name: events[index].name,
+          date: events[index].date,
+          description: events[index].description,
+          img: '/assets/images/Oswego_logo_horizontal_black.png'  //placeholder
+        };
+      });
     });
   }
 
