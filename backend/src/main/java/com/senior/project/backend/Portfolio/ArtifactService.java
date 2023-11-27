c91b0399ffb37474f4af760c03262341eb5557c4
Justin Swistak <jts7382@rit.edu>
2023-11-26 23:13:51 -0500

    file type validation

--- backend/src/main/java/com/senior/project/backend/Portfolio/ArtifactService.java
+++ backend/src/main/java/com/senior/project/backend/Portfolio/ArtifactService.java
@@ -1,8 +1,11 @@
 package com.senior.project.backend.Portfolio;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 import java.nio.file.Path;
@@ -20,13 +23,26 @@ public class ArtifactService {
     public ArtifactService(ArtifactRepository artifactRepository) { this.artifactRepository = artifactRepository;}
 
     public Mono<String> processFile(FilePart filePart) {
-        // TODO Validate file size, type.
+        // TODO Validate file size.
+        String extension;
+
+        // Check if the file is a PDF based on content type
+        MediaType contentType = filePart.headers().getContentType();
+        if (contentType == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No Content type provided for file");
+        } else if (contentType.isCompatibleWith(MediaType.APPLICATION_PDF)){
+            extension = ".pdf";
+        }
+        // Add more file types here
+        else {
+            throw new ResponseStatusException(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "Unsupported filetype");
+        }
 
         // Generate a unique identifier for the file
-        String fileId = UUID.randomUUID().toString();
+        String uniqueFilename = UUID.randomUUID() + extension;
 
         // Construct the destination path using the unique identifier
-        Path destination = Paths.get(uploadDirectory, fileId);
+        Path destination = Paths.get(uploadDirectory, uniqueFilename);
 
         // Save the file to the specified directory
         return filePart.transferTo(destination)
