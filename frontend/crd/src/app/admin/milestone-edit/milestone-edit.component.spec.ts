a59023edd2c900bc5a350236d8fdc0b65dc9465a
bml2238 <bml2238@rit.edu>
2024-01-18 15:58:18 -0500

    added tests

--- frontend/crd/src/app/admin/milestone-edit/milestone-edit.component.spec.ts
+++ frontend/crd/src/app/admin/milestone-edit/milestone-edit.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { MilestoneEditComponent } from './milestone-edit.component';
+import { BehaviorSubject, of } from "rxjs";
 
 describe('MilestoneEditComponent', () => {
   let component: MilestoneEditComponent;
   let fixture: ComponentFixture<MilestoneEditComponent>;
+  const paramMap = new BehaviorSubject(convertToParamMap({ name: 'name'  }));
+  const activatedRouteMock = {
+    params: paramMap.asObservable(),
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [MilestoneEditComponent]
+      declarations: [MilestoneEditComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: activatedRouteMock
+        }
+      ]
     });
     fixture = TestBed.createComponent(MilestoneEditComponent);
     component = fixture.componentInstance;
--- frontend/crd/src/app/admin/milestone-main-page/milestone-main-page.component.spec.ts
+++ frontend/crd/src/app/admin/milestone-main-page/milestone-main-page.component.spec.ts
@@ -1,14 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import createSpyObj = jasmine.createSpyObj;
+import { of } from "rxjs";
+import {Milestone, YearLevel} from "../../../domain/Milestone";
 import { MilestoneMainPageComponent } from './milestone-main-page.component';
+import { MilestoneService } from 'src/app/milestones-page/milestones/milestone.service';
+import { MatCardModule } from "@angular/material/card";
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 describe('MilestoneMainPageComponent', () => {
   let component: MilestoneMainPageComponent;
   let fixture: ComponentFixture<MilestoneMainPageComponent>;
+  let milestoneServiceSpy = createSpyObj('MilestoneService', ['getMilestones']);
+  milestoneServiceSpy.getMilestones.and.returnValue(of(Array(new Milestone({
+    name: "name",
+    yearLevel: YearLevel.Freshman,
+    milestoneID: "id",
+    events: [],
+    tasks: [],
+  }))));
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [MilestoneMainPageComponent]
+      imports: [MatCardModule, MatGridListModule],
+      declarations: [MilestoneMainPageComponent],
+      providers: [{provide: MilestoneService, useValue: milestoneServiceSpy}],
     });
     fixture = TestBed.createComponent(MilestoneMainPageComponent);
     component = fixture.componentInstance;
