7236334cc6bd649ea22989e95c03f9907786cf63
Justin Swistak <jts7382@rit.edu>
2024-01-21 18:47:01 -0500

    don't know how to write test

--- backend/src/test/java/com/senior/project/backend/portfolio/ArtifactHandlerTest.java
+++ backend/src/test/java/com/senior/project/backend/portfolio/ArtifactHandlerTest.java
@@ -8,8 +8,35 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.mock.web.reactive.function.server.MockServerRequest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.MimeType;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ArtifactHandlerTest {
@@ -31,36 +58,51 @@ public class ArtifactHandlerTest {
     }
 
     @Test
-    public void testHandleFileUpload() {
+    public void testHandleFileUpload() throws IOException {
+
+//        IDK how to test this
+
 //        when(artifactService.processFile(any())).thenReturn(Mono.just("test"));
+
+//        ByteArrayResource resource = new ByteArrayResource("contentBytes".getBytes());
 //
+//        MimeType mimeType = MimeTypeUtils.TEXT_PLAIN;
+//        FilePart filePart = new FilePart("file", "filename", resource, mimeType);
+//        return new FormDataPart("file", filePart);
+        // Create FilePart
+//        FilePart filePart = new FilePart("file", resource, resource.getClass(), null);
+//        FilePart filePart = new MockMultipartFile("file", "filename", "text/plain", "contentBytes".getBytes());
 //        FilePart filePart = mock(FilePart.class);
 //        when(filePart.transferTo((File) any())).thenReturn(Mono.empty());
 //        when(filePart.transferTo((Path) any())).thenReturn(Mono.empty());
-//        assert filePart != null;
+
 //        MockServerRequest request = MockServerRequest.builder()
 //                .header(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
 //                .body(BodyInserters.fromMultipartData("file", filePart));
 //
 //        Mono<ServerResponse> responseMono = artifactHandler.handleFileUpload(request);
+//        assertNotNull(responseMono);
+//        StepVerifier.create(responseMono).expectNext("test").expectComplete().verify();
 
-        // Create a MockServerRequest with MultipartData
-//        ServerRequest request = MockServerRequest.builder()
-//                .method(HttpMethod.POST)
-//                .header(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
-//                .body(BodyInserters.fromMultipartData("file", filePart))
-//                .build();
+//        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+//                "multipartFile",
+//                "test.pdf",
+//                "application/pdf",
+//                new ClassPathResource("test.pdf").getInputStream());
 
-
+//        FilePart filePart = mock(FilePart.class);
+//        when(filePart.filename()).thenReturn("TestImage.png");
+//
+//        File file = new File(filePart.filename());
+//        var multi = new MockMultipartFile("name", "content".getBytes());
+//
 //        String result = webTestClient.post()
 //                .uri("/test")
 //                .header(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
-//                .body(temp)
+//                .body(BodyInserters.fromMultipartData("file", multi))
 //                .exchange().expectStatus().isOk()
 //                .expectBody(String.class).returnResult().getResponseBody();
 //        assertNotNull(result);
 //        assertEquals("test", result);
-//        assertEquals(Constants.m1.getId(), result.get(0).getId());
-//        assertEquals(Constants.m2.getId(), result.get(1).getId());
     }
 }
