78627fbdec71295cc403b0e90fb84062b54879c9
Justin Swistak <jts7382@rit.edu>
2024-01-22 17:16:42 -0500

    merge conflict

--- backend/src/main/java/com/senior/project/backend/Portfolio/ArtifactService.java
+++ backend/src/main/java/com/senior/project/backend/Portfolio/ArtifactService.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
