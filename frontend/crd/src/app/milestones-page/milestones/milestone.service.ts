14df103907af2378291219e9dbca5962c38af5b7
bml2238 <bml2238@rit.edu>
2024-01-22 12:25:14 -0500

    simple milestone cache

--- frontend/crd/src/app/milestones-page/milestones/milestone.service.ts
+++ frontend/crd/src/app/milestones-page/milestones/milestone.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Milestone, MilestoneJSON} from "../../../domain/Milestone";
-import {map, Observable} from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Milestone, MilestoneJSON } from "../../../domain/Milestone";
+import { concatMap, finalize, map, Observable, of, ReplaySubject } from "rxjs";
 import { Endpoints, constructBackendRequest } from 'src/app/util/http-helper';
 
 @Injectable({
@@ -9,18 +9,39 @@ import { Endpoints, constructBackendRequest } from 'src/app/util/http-helper';
 })
 export class MilestoneService {
 
+  private milestoneCache$ = new ReplaySubject<any>();
+  private hasBeenRequested = false;
+
   constructor(
     private http: HttpClient,
   ) {
   }
 
-  // Get all data items
-  getMilestones(): Observable<Milestone[]> {
-    return this.http.get<Milestone[]>(constructBackendRequest(Endpoints.MILESTONES))
-      .pipe(map((data: any) => {
-        return data.map((milestoneData: MilestoneJSON) => {
-          return new Milestone(milestoneData)
-        })
-      }))
+  getMilestones(forceRefresh?: boolean): any {
+    // return last value (i.e. cache) from ReplaySubject or add data to it
+    if (!this.hasBeenRequested || forceRefresh) {
+      console.log("calling refresh")
+      this.hasBeenRequested = true;
+      const milestoneData = this.http.get<Milestone[]>(constructBackendRequest(Endpoints.MILESTONES))
+        .pipe(map((data: any) => {
+          return data.map((milestoneData: MilestoneJSON) => {
+            return new Milestone(milestoneData)
+          })
+      }));
+
+      //makes sure the HTTP is done before adding data to cache
+      const processedObservable = milestoneData.pipe(
+          concatMap((processedData: any) => {
+            this.milestoneCache$.next(processedData);
+            return of(processedData);
+          })
+        );
+
+      return processedObservable;
+    }
+
+    return this.milestoneCache$;
+
   }
+
 }
