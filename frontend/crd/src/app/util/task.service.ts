16f95ef8203a7e63ee675dc2cf2edae34d597689
bml2238 <bml2238@rit.edu>
2024-01-23 17:58:50 -0500

    removed console.log

--- frontend/crd/src/app/util/task.service.ts
+++ frontend/crd/src/app/util/task.service.ts
@@ -23,8 +23,6 @@ export class TaskService {
       this.hasBeenRequested = true;
 
       this.http.get<Task[]>(constructBackendRequest(Endpoints.TASKS)).subscribe((data) => {
-        console.log("ooo received data");
-        console.log(data);
         const mappedData = data.map((taskData: any) => {
             return new Task(taskData)
           })
