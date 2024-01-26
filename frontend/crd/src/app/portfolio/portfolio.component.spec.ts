cf3f0b93d353f126a8ee43270266cb2f57ff2655
Justin Swistak <jts7382@rit.edu>
2024-01-26 15:04:06 -0500

    fixed front end tests

--- frontend/crd/src/app/portfolio/portfolio.component.spec.ts
+++ frontend/crd/src/app/portfolio/portfolio.component.spec.ts
@@ -8,11 +8,18 @@ import {MatCardModule} from "@angular/material/card";
 import {MatIconModule} from "@angular/material/icon";
 import {constructBackendRequest, Endpoints} from "../util/http-helper";
 import {FileUploadComponent} from "../file-upload/file-upload.component";
+import {ArtifactService} from "./artifact.service";
+import {of} from "rxjs";
 
 describe('PortfolioComponent', () => {
   let component: PortfolioComponent;
   let fixture: ComponentFixture<PortfolioComponent>;
+  let matDialogRef = jasmine.createSpyObj('MatDialogRef', ['afterClosed'])
+  matDialogRef.afterClosed.and.returnValue(of())
   let matDialog = jasmine.createSpyObj('MatDialog', ['open']);
+  matDialog.open.and.returnValue(matDialogRef)
+  let artifactSvc = jasmine.createSpyObj('ArtifactService', ['getPortfolioArtifacts']);
+  artifactSvc.getPortfolioArtifacts.and.returnValue(of( ))
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -22,7 +29,8 @@ describe('PortfolioComponent', () => {
       ],
       imports: [MatCardModule, MatIconModule],
       providers: [
-        {provide: MatDialog, useValue: matDialog}
+        {provide: MatDialog, useValue: matDialog},
+        {provide: ArtifactService, useValue: artifactSvc},
       ]
     });
     fixture = TestBed.createComponent(PortfolioComponent);
