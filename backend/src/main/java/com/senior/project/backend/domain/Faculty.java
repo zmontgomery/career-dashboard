658734b9113b95f5157c5a7775854b1a2121e63e
Riley Brotz <rcb2957@rit.edu>
2024-01-28 00:30:59 -0500

    Updated Faculty domain class

--- backend/src/main/java/com/senior/project/backend/domain/Faculty.java
+++ backend/src/main/java/com/senior/project/backend/domain/Faculty.java
@@ -1,16 +1,24 @@
 package com.senior.project.backend.domain;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
+@Entity
 @Data
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Generated
-public class Faculty {
+public class Faculty extends User{
 
-    private String id;
+	@Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 	
-	private String userID;
 	private boolean isAdmin;
 }
--- backend/src/main/java/com/senior/project/backend/domain/Student.java
+++ backend/src/main/java/com/senior/project/backend/domain/Student.java
@@ -3,18 +3,28 @@ package com.senior.project.backend.domain;
 import lombok.*;
 
 import java.util.Date;
+import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 @Data
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Generated
-public class Student {
+public class Student extends User{
 	
-    private String id;
+	@Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 	
 	private int userID;
 	private int universityID;
+
 	private Date graduationYear;
 	private Date startDate;
 }
