f4c8b0700dff8c8e8b328fb9f58ad48b90438b6d
Riley Brotz <rcb2957@rit.edu>
2024-02-08 18:08:59 -0500

    Pulled from main

--- backend/src/main/java/com/senior/project/backend/domain/User.java
+++ backend/src/main/java/com/senior/project/backend/domain/User.java
@@ -23,7 +23,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
