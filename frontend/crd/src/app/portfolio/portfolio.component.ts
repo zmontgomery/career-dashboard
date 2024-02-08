60efa0be6acec8a47d343b26c9467f18d7b5359f
Riley Brotz <rcb2957@rit.edu>
2024-02-08 18:49:05 -0500

    Name shows up on portfolio page

--- frontend/crd/src/app/portfolio/portfolio.component.ts
+++ frontend/crd/src/app/portfolio/portfolio.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import {FileUploadComponent} from "../file-upload/file-upload.component";
 import {constructBackendRequest, Endpoints} from "../util/http-helper";
-import {User} from "../../domain/User";
+import { AuthService } from '../security/auth.service';
+import { LangUtils } from '../util/lang-utils';
+import { User } from '../security/domain/user';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.less']
 })
-export class PortfolioComponent {
-  constructor(public dialog: MatDialog) {}
+export class PortfolioComponent implements OnInit{
+  constructor(public dialog: MatDialog, public authService: AuthService) {}
+
+  user: User = User.makeEmpty();
+
+  ngOnInit(): void {
+    this.authService.user$.subscribe((user) => {
+      if (LangUtils.exists(user)) {
+        this.user = user!;
+      }
+    });
+  }
 
   openDialog(): void {
     this.dialog.open(FileUploadComponent, {
