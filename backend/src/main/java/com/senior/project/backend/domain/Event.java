4b5e623f997d15f6475180ba462a75762fb87550
Justin Swistak <jts7382@rit.edu>
2024-02-20 19:10:14 -0500

    merge conflicts

--- backend/src/main/java/com/senior/project/backend/domain/Event.java
+++ backend/src/main/java/com/senior/project/backend/domain/Event.java
@@ -1,6 +1,8 @@
 package com.senior.project.backend.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
