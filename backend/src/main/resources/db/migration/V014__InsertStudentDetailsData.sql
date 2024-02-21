96f1900f73599ee5827e0e14d5683a974a8c376f
Riley Brotz <rcb2957@rit.edu>
2024-02-21 01:17:33 -0500

    Created separate data for each teammate

--- backend/src/main/resources/db/migration/V014__InsertStudentDetailsData.sql
+++ backend/src/main/resources/db/migration/V014__InsertStudentDetailsData.sql
@@ -1,24 +1,36 @@
-INSERT INTO student_details(id, university_id, gpa, description, graduation_year, start_date, year_level) 
-VALUES (UUID_TO_BIN(UUID()), '2', 3.600, 'student', null, null, 'Senior'),
-    (UUID_TO_BIN(UUID()), '3', 3.600, 'student', null, null, 'Senior'),
-    (UUID_TO_BIN(UUID()), '4', 3.600, 'student', null, null, 'Senior'),
-    (UUID_TO_BIN(UUID()), '5', 3.600, 'student', null, null, 'Senior');
+SET @justinId = UUID_TO_BIN(UUID());
+SET @brieId = UUID_TO_BIN(UUID());
+SET @johnId = UUID_TO_BIN(UUID());
+SET @rileyId = UUID_TO_BIN(UUID());
 
-SELECT @userId := id FROM user WHERE user.email="rcb2957@g.rit.edu";
+INSERT INTO student_details(id, university_id, gpa, description, graduation_year, start_date, year_level) 
+VALUES (@justinId, '2', 3.600, 'student', null, null, 'Senior'),
+    (@brieId, '3', 3.600, 'student', null, null, 'Senior'),
+    (@johnId, '4', 3.600, 'student', null, null, 'Senior'),
+    (@rileyId, '5', 3.600, 'student', null, null, 'Senior');
+
+SET @userId = (SELECT id FROM user WHERE user.email="jrl9984@g.rit.edu");
+SET @justinUserId = (SELECT id FROM user WHERE user.email="jts7382@g.rit.edu");
+SET @brieUserId = (SELECT id FROM user WHERE user.email="bml2238@g.rit.edu");
+SET @johnUserId = (SELECT id FROM user WHERE user.email="jld7456@g.rit.edu");
+SET @rileyUserId = (SELECT id FROM user WHERE user.email="rcb2957@g.rit.edu");
 
 UPDATE user
 SET user.student_details_id = @id
 WHERE user.id = @userId ;
 
 UPDATE user
-SET user.is_student = 1
-WHERE user.id = @userId ;
+SET user.student_details_id = @justinId
+WHERE user.id = @justinUserId ;
 
 UPDATE user
-SET user.is_faculty = 1
-WHERE user.id = @userId ;
+SET user.student_details_id = @brieId
+WHERE user.id = @brieUserId ;
 
 UPDATE user
-SET user.is_admin = 1
-WHERE user.id = @userId ;
+SET user.student_details_id = @johnId
+WHERE user.id = @johnUserId ;
 
+UPDATE user
+SET user.student_details_id = @rileyId
+WHERE user.id = @rileyUserId ;
--- backend/src/main/resources/db/migration/V019__AddJob.sql
+++ backend/src/main/resources/db/migration/V019__AddJob.sql
@@ -1,4 +1,4 @@
 INSERT INTO job(id, student_details_id, name, location, description, start_date, end_date, is_coop)
-VALUES (UUID_TO_BIN(UUID()), @studentId, 'Software Engineer I', 'Rochester, NY', 'wrote automated tests', '2021-01-01', '2021-05-05', 0),
-    (UUID_TO_BIN(UUID()), @studentId, 'Software Engineer I', 'Buffalo, NY', 'developed n environment dashboard', '2021-01-01', '2021-05-05', 1),
-    (UUID_TO_BIN(UUID()), @studentId, 'Software Engineer I', 'Rochester, NY', 'wrote automated tests', '2021-01-01', '2021-05-05', 0);
+VALUES (UUID_TO_BIN(UUID()), @studentId, 'Software Engineer I', 'Rochester, NY', 'developed an authentication service', '2024-09-20', '2024-05-10', 0),
+    (UUID_TO_BIN(UUID()), @studentId, 'Software Engineer Intern', 'Philadelphia, PA', 'wrote automated tests', '2021-01-01', '2021-05-05', 1),
+    (UUID_TO_BIN(UUID()), @studentId, 'Software Engineer II', 'Rochester, NY', 'developed a database for the IRS', '2022-01-01', '2021-05-05', 0);
