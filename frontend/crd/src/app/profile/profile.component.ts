3e425952e6b379c2f6e047fc3b62e08d2bcaf4c4
Riley Brotz <rcb2957@rit.edu>
2024-02-23 01:34:42 -0500

    Make profile page display some data

--- frontend/crd/src/app/profile/profile.component.ts
+++ frontend/crd/src/app/profile/profile.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LangUtils } from '../util/lang-utils';
+import { User } from '../security/domain/user';
+import { AuthService } from '../security/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.less']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
 
+  user: User = User.makeEmpty();
+
+  constructor(
+    private readonly authService: AuthService
+  ) {}
+  
+  ngOnInit(): void {
+    this.authService.user$.subscribe((user) => {
+      if (LangUtils.exists(user)) {
+        this.user = user!;
+      }
+    });
+  }
 }
