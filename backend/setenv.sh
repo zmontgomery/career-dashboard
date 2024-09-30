21253dbae79d7ba736994393f8785ede2d2547ee
Greg Villafane <gjv4512@g.rit.edu>
2024-09-30 12:53:40 -0400

    reverting to original

--- backend/setenv.sh
+++ backend/setenv.sh
@@ -15,7 +15,7 @@ read -p "Enter the database username (default: backend): " CRD_DB_USERNAME
 CRD_DB_USERNAME=${CRD_DB_USERNAME:-backend}
 
 read -p "Enter the database password (default: your_password): " CRD_DB_PASSWORD
-CRD_DB_PASSWORD=${CRD_DB_PASSWORD:-Abort-never9rely}
+CRD_DB_PASSWORD=${CRD_DB_PASSWORD:-your_password}
 
 read -p "Enter the super admin: " CRD_SUPER_ADMIN
 
@@ -24,10 +24,10 @@ if [ "$CRD_SUPER_ADMIN" = "" ]; then
     exit 1
 fi
 read -p "Enter the gmail account username (default: partiallyhydrateddevs@gmail.com): " EMAIL_USERNAME
-EMAIL_USERNAME=${EMAIL_USERNAME:-harborfest40@gmail.com}
+EMAIL_USERNAME=${EMAIL_USERNAME:-partiallyhydrateddevs@gmail.com}
 
 read -p "Enter the gmail account password (default: your_password): " EMAIL_PASSWORD
-EMAIL_PASSWORD=${EMAIL_PASSWORD:-Oswego2024!!}
+EMAIL_PASSWORD=${EMAIL_PASSWORD:-your_password}
 
 
 export CRD_DB_HOST
